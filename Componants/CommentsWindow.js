8400ba4688a8251970c6139e44e8950d078ab762
Mohamed Laghzali <75735@office.wsm.warszawa.pl>
2022-07-13 16:47:14 +0200

    KeyboardAreaView

--- Componants/CommentsWindow.js
+++ Componants/CommentsWindow.js
@@ -4,84 +4,85 @@ import { TextInput } from 'react-native-paper';
 import { Ionicons } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
 
-import { StyleSheet, TouchableHighlight, ScrollView, TouchableOpacity, View, Text, Button, Dimensions } from 'react-native';
+import { StyleSheet, TouchableHighlight, ScrollView, KeyboardAvoidingView, TouchableOpacity, View, Text, Button, Dimensions } from 'react-native';
 import { Avatar } from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
 const CommentsWindodw = ({ navigation, route }) => {
     const [comment, setComment] = React.useState("");
     const post = route.params.post
     const emptySpace = "" // for the sake of space-between
     console.log(post.comments.coms)
     return (
-        <View style={styles.container}>
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
 
-            <View style={styles.Header}>
+            <SafeAreaView style={styles.Header}>
                 <TouchableHighlight underlayColor='none' style={styles.backButton} onPress={() => navigation.goBack()}>
                     <Ionicons name="arrow-back" size={30} color="black" />
                 </TouchableHighlight>
-                <Text style={{ fontWeight: 'bold', fontSize: 17 }}>Comments</Text>
+                <Text style={{ fontWeight: 'bold', fontSize: 17, marginTop: 15 }}>Comments</Text>
                 <Text>{emptySpace}</Text>
-            </View>
+            </SafeAreaView>
 
-            <Divider></Divider>
+            <Divider style={{ marginBottom: 10 }}></Divider>
 
-            <View style={styles.Body}>
-                <ScrollView contentContainerStyle={{ flex: 3, flexDirection: 'column' }}>
+            <ScrollView style={styles.Body}>
 
-                    {post.comments.coms.map((comment) => {
-                        return (<View key={comment.id} style={styles.comment}>
-                            <Avatar.Image size={30} source={require('../assets/profile.jpg')} />
-                            <Text style={{ alignSelf: 'flex-start', fontWeight: 'bold', marginLeft: 5 }}>{comment.uid}</Text>
-                            <Text style={{ alignSelf: 'flex-start', marginLeft: 5 }}>{comment.text}</Text>
-                            <View style={{ marginLeft: 'auto', alignItems: 'center' }}>
-                                {/* grab isLiked and changed color based on that isLiked ? 'tomato' : 'white'*/}
-                                <TouchableOpacity>
-                                    <AntDesign name="like1" size={15} color={comment.isLiked ? 'tomato' : 'grey'} />
-                                </TouchableOpacity>
-                            </View>
-                        </View>)
-                    })}
-                </ScrollView>
-            </View>
+                {post.comments.coms.map((comment) => {
+                    return (<View key={comment.id} style={styles.comment}>
+                        <Avatar.Image size={30} source={require('../assets/profile.jpg')} />
+                        <Text style={{ alignSelf: 'flex-start', fontWeight: 'bold', marginLeft: 5 }}>{comment.uid}</Text>
+                        <Text style={{ alignSelf: 'flex-start', marginLeft: 5 }}>{comment.text}</Text>
+                        <View style={{ marginLeft: 'auto', alignItems: 'center' }}>
+                            {/* grab isLiked and changed color based on that isLiked ? 'tomato' : 'white'*/}
+                            <TouchableOpacity>
+                                <AntDesign name="like1" size={15} color={comment.isLiked ? 'tomato' : 'grey'} />
+                            </TouchableOpacity>
+                        </View>
+                    </View>)
+                })}
+            </ScrollView>
 
-            <View style={styles.footer}>
+            <SafeAreaView style={styles.footer}>
                 <View style={styles.TextArea}>
                     <Avatar.Image size={35} source={require('../assets/profile.jpg')} />
                     <TextInput
                         style={styles.commentInput}
-                        label="Comment"
 
                         value={comment}
+                        mode={"flat"}
                         onChangeText={comment => setComment(comment)}
                         right={< TextInput.Icon size={30} onPress={() => alert('dd?')} color={"tomato"} multiline={true} name="send-circle" />}
                     />
                 </View>
-            </View>
-        </View>
+            </SafeAreaView>
+        </KeyboardAvoidingView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-
-        height: Dimensions.get('window').height
+        flex: 1,
     },
     Header: {
-        marginTop: 20,
+        alignItems: 'flex-end',
+
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
 
     },
     backButton: {
-        marginLeft: 10
+        marginLeft: 10,
+        marginTop: 15
     },
     Body: {
-
+        flex: 1,
+        flexDirection: 'column'
     },
     footer: {
-        bottom: 0,
+
         width: '100%',
-        position: 'absolute'
+        marginBottom: 'auto'
     },
     TextArea: {
         padding: 10,
@@ -99,10 +100,10 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-start'
     }
     , commentInput: {
-        borderColor: "grey",
+        marginLeft: 5,
+
         width: "90%",
-        borderWidth: 1,
-        borderRadius: 10,
+
         marginLeft: '10px'
     }
 })
